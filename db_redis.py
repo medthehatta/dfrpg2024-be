79a2ab384c6d04d8ce466cc102e7f5355e239a2e
Med Mahmoud <medthehatta@gmail.com>
2024-07-10 23:29:22 +0000

    Fix redis networking in compose

--- db_redis.py
+++ db_redis.py
@@ -1,12 +1,13 @@
 import os
 from redis import Redis
 
+redis_host = os.environ.get("REDIS_HOST", "localhost")
 redis_port = os.environ.get("REDIS_PORT", "6379")
 
 redis_password = os.environ["REDIS_PASSWORD"]
 
 redis = Redis(
-    host="localhost",
+    host=redis_host,
     port=redis_port,
     db=0,
     password=redis_password,
